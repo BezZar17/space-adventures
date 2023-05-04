9bbc7a763874a362c4a9e528c55edee87ad4f06b
Cesar BezZar17 <ccruz2408@outlook.com>
2023-05-03 21:10:16 -0700

    remoding css, board and img created

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
 
+//board
+let board
+let boardWidth = 800
+let boardHeight = 500
+let context
+
+
+//spaceship
+let shipWidth = 100
+let shipHeight = 75
+let shipX = boardWidth/8
+let shipY = boardHeight/2
+let shipImg
+
+let ship = {
+    x : shipX,
+    y : shipY,
+    width : shipWidth,
+    height : shipHeight
+}
+window.onload = function(){
+    board = document.getElementById("board")
+    board.height = boardHeight
+    board.width = boardWidth
+    context = board.getContext("2d") //for drawing on the board
+
+    //load img
+    shipImg = new Image()
+    shipImg.src = "/assets/images/pngegg.png"
+    shipImg.onload = function() {
+        context.drawImage(shipImg, ship.x, ship.y, ship.width, ship.height)
+    }
+}
