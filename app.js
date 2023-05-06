f1682b3b42ec4c0ba14623cb7b0f413eb9862c85
Cesar BezZar17 <ccruz2408@outlook.com>
2023-05-06 11:06:11 -0700

    created top asteriod motion

--- app.js
+++ app.js
@@ -19,16 +19,72 @@ let ship = {
     width : shipWidth,
     height : shipHeight
 }
+
+//asteriod
+
+let asteriodArray= []
+let asteriodWidth = 150
+let asteriodHieght = 200
+let asteriodX = boardWidth
+let asteriodY = 0
+
+let topAsteriodImg
+let bottomAsteriodImg
+
+//physics
+
+let velocityX = -2 // asteriods moving left
+
 window.onload = function(){
     board = document.getElementById("board")
     board.height = boardHeight
     board.width = boardWidth
     context = board.getContext("2d") //for drawing on the board
 
-    //load img
+    //load imgs
     shipImg = new Image()
     shipImg.src = "/assets/images/pngegg.png"
     shipImg.onload = function() {
         context.drawImage(shipImg, ship.x, ship.y, ship.width, ship.height)
     }
+
+    topAsteriodImg = new Image()
+    topAsteriodImg.src = "/assets/images/asteriod.png"
+
+    bottomAsteriodImg = new Image()
+    bottomAsteriodImg.src = "/assets/images/asteriod.png"
+
+    requestAnimationFrame(update);
+    setInterval(placeAsteriods, 2000) //every 2 seconds
+}
+
+function update(){
+    requestAnimationFrame(update);
+    context.clearRect(0, 0, board.width, board.height)
+
+
+//spaceship
+context.drawImage(shipImg, ship.x, ship.y, ship.width, ship.height)
+
+//asteriod
+ for(let i = 0; i < asteriodArray.length; i++){
+    let asteriod = asteriodArray[i]
+    asteriod.x += velocityX
+    context.drawImage(asteriod.img, asteriod.x, asteriod.y, asteriod.width, asteriod.height)
+ }
+
+}
+
+function placeAsteriods(){
+    let topAsteriod = {
+        img : topAsteriodImg,
+        x : asteriodX,
+        y : asteriodY,
+        width : asteriodWidth,
+        height : asteriodHieght,
+        passed : false
+    }
+
+    asteriodArray.push(topAsteriod)
+
 }
